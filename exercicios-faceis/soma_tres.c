476d415e670c2d37ec2997e6ad3e8609f059f4fe
Ingrid Renata Rodrigues <ingridrenata.dev@gmail.com>
2026-07-23 19:31:49 +0000

    feat: exercicios faceis focando em operações e lógica.

---
+++ exercicios-faceis/soma_tres.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int localizador_maior(int lista[], int tamanho){
+    int maior = lista[0];
+    
+    for(int i = 1; i < tamanho; i++){
+        
+        if(maior < lista[i]){
+            maior = lista[i];
+        }
+    }
+
+    return maior;
+}
+
+int main()
+{
+    int lista[5];
+    int tamanho = 5;
+    
+    for(int i = 0; i < tamanho; i++){
+        printf("numero: ");
+        scanf("%d", &lista[i]);
+    }
+    
+    int resultado = localizador_maior(lista, tamanho);
+    printf("O resultado eh: %d\n", resultado);
+
+    return 0;
+}
